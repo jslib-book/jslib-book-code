a167b4b5907a42b329c35b81e056159b8145cb17
yanhaijing <yanhaijing@yeah.net>
2020-11-28 21:05:07 +0800

    demo: up

--- cp3/clone/test/test.js
+++ cp3/clone/test/test.js
@@ -1,9 +1,31 @@
 var expect = require("expect.js");
+var clone = require("../dist/index.js").clone;
 
-describe("单元测试", function () {
-    describe("test hello", function () {
-        it("hello", function () {
-            expect(1).to.equal(1);
+describe("function clone", function () {
+    describe("param data", function () {
+        it("正确用例", function () {
+            // 基本数据类型
+            expect(clone("abc")).to.equal("abc");
+
+            // 数组
+            var arr = [1, [2]];
+            var cloneArr = clone(arr);
+            expect(cloneArr).not.to.equal(arr);
+            expect(cloneArr).to.eql(arr);
+
+            // 对象
+            var obj = { a: { b: 1 } };
+            var cloneObj = clone(obj);
+            expect(cloneObj).not.to.equal(obj);
+            expect(cloneObj).to.eql(obj);
+        });
+
+        it("边界值用例", function () {
+            expect(clone()).to.equal(undefined);
+
+            expect(clone(undefined)).to.equal(undefined);
+
+            expect(clone(null)).to.equal(null);
         });
     });
 });
